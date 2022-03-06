3386606d7c1efa3b313afc37a8cd4983bf36e571
Utkrist <utkrist991@gmail.com>
2022-03-06 23:48:35 +0545

    Structured Files

--- mainProj/urls.py
+++ mainProj/urls.py
@@ -1,10 +1,12 @@
 import imp
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 from mainProj import views
 urlpatterns = [
     path('', views.index, name="home"),
-    path('about', views.about, name="about"), 
-    path('previousbookings', views.prevBookings, name="PreviousBookings") 
+    path('futsals', views.futsals, name="futsals"), 
+    path('previousbookings', views.prevBookings, name="PreviousBookings"),
+    path('authentication', views.authentication, name="authentication")
 
 ]
--- mainProj/views.py
+++ mainProj/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
-    return HttpResponse("Home@proj")
+ #   return HttpResponse("Home@proj")
 
-def about(request):
-    return HttpResponse("About@proj")
+def futsals(request):
+        return render(request, 'futsals.html')
 
-def prevBookings(requests):
-    return HttpResponse("Previous_Bookings@proj")
+def prevBookings(request):
+    return render(request, 'previousbookings.html')
+
+def authentication(request):
+    return render(request, 'authentications.html')

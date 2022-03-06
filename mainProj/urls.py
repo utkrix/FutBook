3ebbe38297ae1f763220d5f47eff4152312b94d9
Utkrist <utkrist991@gmail.com>
2022-03-06 22:48:41 +0545

    Files Structed

--- mainProj/urls.py
+++ mainProj/urls.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from mainProj import views
 urlpatterns = [
-    path('', views.index, name="home") 
+    path('', views.index, name="home"),
+    path('about', views.about, name="about"), 
+    path('previousbookings', views.prevBookings, name="PreviousBookings") 
+
 ]
--- mainProj/views.py
+++ mainProj/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def index(requests):
+def index(request):
+    return render(request, 'index.html')
     return HttpResponse("Home@proj")
+
+def about(request):
+    return HttpResponse("About@proj")
+
+def prevBookings(requests):
+    return HttpResponse("Previous_Bookings@proj")
